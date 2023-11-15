e9a9baf01a91c8d61c830cdfccedcd66902b5e99
Jacob Poling <polingj999@gmail.com>
2023-11-15 13:24:50 -0800

    Fixed linting issues suggested by pylint cli, resolves #2

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,12 +1,14 @@
-# Import flask module
+"""This is the Flask Api"""
 from flask import Flask
- 
 app = Flask(__name__)
- 
 @app.route('/')
 def index():
+    """Root API ROUTE
+
+    Returns:
+        String: Just a Hello world string!
+    """
     return '<h1>Hello from our flask server!</h1>'
- 
 # main driver function
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
