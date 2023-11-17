ae8a33605ae7ac4e44aa60ff38f34d5e3e9c04c9
Jacob Poling <polingj999@gmail.com>
2023-11-16 20:19:45 -0800

    fixed failed test

--- flask-api/app.py
+++ flask-api/app.py
@@ -9,7 +9,7 @@ def index():
     Returns:
         String: Just a Hello world string!
     """
-    return '<h1>Hello from our flask server</h1>'
+    return '<h1>Hello from our flask server!</h1>'
 # main driver function
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
