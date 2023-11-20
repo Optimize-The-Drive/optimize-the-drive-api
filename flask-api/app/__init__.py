b29f55024b61bf3a83ff7a9b7a157ecbe9ed95b0
Nathan Dunn <ndunn2230@gmail.com>
2023-11-19 20:28:43 -0800

    Added support for loading environment config

--- flask-api/app/__init__.py
+++ flask-api/app/__init__.py
@@ -1,11 +1,27 @@
+'''
+    Creates an instance of the flask application
+'''
 from flask import Flask
 from app.routes import api_routes
+from config import get_environment
 
-def create_app():
+api_env = get_environment()
+
+def create_app(config=api_env):
     '''
         Creates an instance of the Flask App
     '''
     app = Flask(__name__)
+    app.config.from_object(config)
     app.register_blueprint(api_routes)
+    register_error_routes(app)
 
     return app
+
+def register_error_routes(app):
+    '''
+        Assigns handlers for common API error codes.
+    '''
+    @app.errorhandler(404)
+    def not_found(e):
+        return 'Not Found :/'
