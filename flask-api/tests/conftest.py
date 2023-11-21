71056a5435443433ccbd6daf28e6f529a0d2b620
Nathan Dunn <ndunn2230@gmail.com>
2023-11-20 21:41:22 -0800

    Fixed tests

---
+++ flask-api/tests/conftest.py
@@ -0,0 +1,22 @@
+"""Test Script for app.py"""
+import pytest
+
+from app import create_app # pylint: disable=E0401
+
+
+@pytest.fixture
+def test_app():
+    """Create and configure a new app instance for each test."""
+    # create the app with common test config
+    app = create_app()
+    yield app
+
+@pytest.fixture
+def client(test_app):
+    """A test client for the app."""
+    return test_app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
--- flask-api/tests/test_app.py
+++ flask-api/tests/test_app.py
@@ -1,9 +1,6 @@
-"""Test Script for app.py"""
-from app import app
-def test_home_route():
-    """tests if the api retruens a 200 code and the correct text
-    """
-    response = app.test_client().get('/')
+import pytest
+
+def test_home_route(client):
+    """A test client for the app."""
+    response = client.get('/api/')
     assert response.status_code == 200
-    assert b"<h1>Hello from our flask server!</h1>" in response.data
-test_home_route()
