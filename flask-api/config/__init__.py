aa10362a1e6b829f961de33fbafda78c36754541
Nathan Dunn <ndunn2230@gmail.com>
2023-11-21 20:09:52 -0800

    Postgres container created

--- flask-api/config/__init__.py
+++ flask-api/config/__init__.py
@@ -13,31 +13,26 @@ class Config(object):
     '''
         Shared config across all environments
     '''
-    DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    POSTGRES_URL = os.getenv('POSTGRES_URL')
 
 class DevConfig(Config):
     '''
         Config for Dev Environment
     '''
-    POSTGRES_URL = os.getenv('POSTGRES_URL')
+    DEBUG = True
 
 class TestConfig(Config):
     '''
         Config for Test Environment
     '''
     TESTING = True
-    POSTGRES_URL = os.getenv('POSTGRES_URL')
 
 def get_environment():
     '''
         Returns the appropriate environment configuration.
     '''
     env = os.getenv('ENV_TYPE')
-    
-    test = os.getenv('SECRET_1')
-    test1 = os.getenv('SECRET_CI_KEY')
-    print(test, test1)
 
     if env == 'dev':
         return DevConfig
