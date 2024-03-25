95202281ad86df56b1609a68832113667c2a0abd
Nathan Dunn <ndunn2230@gmail.com>
2024-03-24 17:48:36 -0700

    Revert back test to pass

--- flask-api/tests/routes/test_trip_routes.py
+++ flask-api/tests/routes/test_trip_routes.py
@@ -10,4 +10,4 @@ def test_message(auth_sio_client):
     """
         Tests that the client can access SocketIO with the correct credentials.
     """
-    assert not auth_sio_client.is_connected()
+    assert auth_sio_client.is_connected()
