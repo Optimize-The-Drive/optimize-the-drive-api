a5a6256322d9c08f85e486b0fa42043b87e607e7
Nathan Dunn <ndunn2230@gmail.com>
2024-03-27 19:44:51 -0700

    Added remaining tests for the trip create/edit routes

--- flask-api/tests/repos/test_trip_repo.py
+++ flask-api/tests/repos/test_trip_repo.py
@@ -11,7 +11,7 @@ from tests.helpers import add_user_to_db, trip_repo, remove_user_from_db
 @pytest.mark.usefixtures("app_ctx")
 def test_by_user_id():
     '''
-        Tests that a Trip can be queried it's users
+        Tests that a Trip can be queried its user's id
     '''
     user = add_user_to_db('user_test_trip', 'user_test_trip@email.com', 'testpassword')
 
@@ -30,3 +30,19 @@ def test_by_user_id():
     assert len(trips_none) == 0
 
     remove_user_from_db(user)
+
+
+@pytest.mark.usefixtures("app_ctx")
+def test_by_id():
+    '''
+        Tests that a Trip can be queried its id
+    '''
+
+    user = add_user_to_db('user_test_trip', 'user_test_trip@email.com', 'testpassword')
+    trip = Trip(name="trip_one", user_id=user.id)
+    trip_repo.add(trip)
+    trip_repo.commit()
+
+    queried_trip = trip_repo.by_id(trip.id)
+
+    assert trip.id == queried_trip.id
