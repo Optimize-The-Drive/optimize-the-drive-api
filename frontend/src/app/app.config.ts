e7ab2570ebffc80dd6b00aa97f523bc6c16af46c
Nathan Dunn <ndunn2230@gmail.com>
2024-04-06 17:25:22 -0700

    Removed ssr from angular app

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(BrowserModule)]
+  providers: [provideRouter(routes)]
 };
